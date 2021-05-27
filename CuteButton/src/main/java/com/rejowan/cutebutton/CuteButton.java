a60c275deeb5eebd1ecf2121fe037a554722d9a5
K M Rejowan Ahmmed <ahmmedrejowan@gmail.com>
2021-05-28 02:22:18 +0600

    Minor Changes

--- CuteButton/src/main/java/com/rejowan/cutebutton/CuteButton.java
+++ CuteButton/src/main/java/com/rejowan/cutebutton/CuteButton.java
@@ -2,7 +2,5 @@ package com.rejowan.cutebutton;
 
 public class CuteButton {
 
-    // Testing Walkatime Dashboard working or not
-
 
 }
--- CuteButton/src/main/java/com/rejowan/cutebutton/DrawableHelper.java
+++
@@ -1,4 +0,0 @@
-package com.rejowan.cutebutton;
-
-public class DrawableHelper {
-}
