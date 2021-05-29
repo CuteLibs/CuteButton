ca490adf696e9ae8afcb566fcd648262835c5305
K M Rejowan Ahmmed <ahmmedrejowan@gmail.com>
2021-05-30 02:52:46 +0600

    Minor Changes

--- CuteButton/src/main/java/com/rejowan/cutebutton/CuteButton.java
+++ CuteButton/src/main/java/com/rejowan/cutebutton/CuteButton.java
@@ -25,6 +25,32 @@ import androidx.annotation.DrawableRes;
 public class CuteButton extends LinearLayout {
 
 
+    /**
+     * CuteButton Library for Android
+     * Created by K M Rejowan Ahmmed (ahmmedrejowan)
+     * Min Api 21
+     * Target Api 30
+     * Java Version 1.8
+     */
+
+
+    /*
+     Copyright 2021 K M Rejowan Ahmmed (ahmmedrejowan)
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+     */
+
+
     public static final int POSITION_LEFT = 1;
     public static final int POSITION_RIGHT = 2;
     public static final int POSITION_TOP = 3;
